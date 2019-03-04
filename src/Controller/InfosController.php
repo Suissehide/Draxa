9b2ff2da66f79b287fde74fc06a687090e5b7dee
leo.couffinhal1@gmail.com <leo.couffinhal@epitech.eu>
2019-03-04 20:47:39 +0100

    push

--- src/Controller/InfosController.php
+++ src/Controller/InfosController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Patient;
 use App\Entity\Infos;
 use App\Form\InfosType;
 use App\Repository\InfosRepository;
@@ -9,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * @Route("/infos")
