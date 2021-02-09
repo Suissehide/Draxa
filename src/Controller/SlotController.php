85a793818159112242a1bf1eb0be7ebc10b43b90
Leo Couffinhal <leo.couffinhal@qwetle.fr>
2021-02-09 16:15:46 +0900

    Fix : change Thematique to Categorie

--- src/Controller/SlotController.php
+++ src/Controller/SlotController.php
@@ -79,7 +79,7 @@ class SlotController extends AbstractController
                         $rendezVous = new RendezVous();
                         $rendezVous->setDate($new_date);
                         $rendezVous->setHeure(\DateTime::createFromFormat('H:i', $heureDebut));
-                        $rendezVous->setThematique($thematique);
+                        $rendezVous->setCategorie($thematique);
                         $rendezVous->setPatient($patient);
                         $slot->addRendezVous($rendezVous);
                     }
@@ -137,7 +137,7 @@ class SlotController extends AbstractController
                         $rendezVous = new RendezVous();
                         $rendezVous->setDate($slot->getDate());
                         $rendezVous->setHeure(\DateTime::createFromFormat('H:i', $heureDebut));
-                        $rendezVous->setThematique($thematique);
+                        $rendezVous->setCategorie($thematique);
                         $rendezVous->setPatient($patient);
                         $slot->addRendezVous($rendezVous);
                     }
