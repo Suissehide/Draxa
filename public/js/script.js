5c3c19b283f4a6afad6ba4bb92ab3d9ae329dfa0
Leo Couffinhal <leo.couffinhal@qwetle.fr>
2025-05-13 22:25:02 +0200

    [ADD] Hide banner for 1h

--- public/js/script.js
+++ public/js/script.js
@@ -2,72 +2,87 @@
  * Script jQuery
  */
 
-jQuery(document).ready(function ($) {
+console.log("ready!");
 
-    console.log("ready!");
+$('.datepicker').datepicker({
+    language: 'fr',
+    format: 'dd/mm/yyyy',
+    todayBtn: true,
+    todayHighlight: true,
+    autoclose: true
+});
 
-    $('.datepicker').datepicker({
-        language: 'fr',
-        format: 'dd/mm/yyyy',
-        todayBtn: true,
-        todayHighlight: true,
-        autoclose: true,
-    });
+/**
+ * Smooth scroll
+ */
 
-	/**
-	* Smooth scroll
-	*/
+const banner = document.getElementById('banner');
+const hideUntil = localStorage.getItem('hideNotificationUntil');
+const now = Date.now();
 
-    $('.js-scrollTo').on('click', function () {
-        var page = $(this).attr('href'); // Page cible
-        var speed = 650; // Durée de l'animation (en ms)
-        $('html, body').animate({ scrollTop: $(page).offset().top - 60 }, speed); // Go
+if (!hideUntil || now >= Number(hideUntil)) {
+    banner.style.display = 'block';
+}
+
+$('#banner').on('click', 'button', function () {
+    banner.style.display = 'none';
+    const delay = Date.now() + (60 * 60 * 1000);
+    localStorage.setItem('hideNotificationUntil', delay.toString());
+})
+
+/**
+* Smooth scroll
+*/
+
+$('.js-scrollTo').on('click', function () {
+    const page = $(this).attr('href'); // Page cible
+    const speed = 650; // Durée de l'animation (en ms)
+    $('html, body').animate({ scrollTop: $(page).offset().top - 60 }, speed); // Go
+    return false;
+});
+
+/**
+ * multipage form
+ */
+
+$('.nav-tabs > li a[title]').tooltip();
+
+$('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+    let target = $(e.target);
+    if (target.parent().hasClass('disabled')) {
         return false;
-    });
-
-	/**
-	 * multi page form
-	 */
-
-    $('.nav-tabs > li a[title]').tooltip();
-
-    $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
-        var $target = $(e.target);
-        if ($target.parent().hasClass('disabled')) {
-            return false;
-        }
-    });
-    $(".next-step").click(function (e) {
-        var $active = $('.wizard .nav-tabs li.active');
-        $active.next().removeClass('disabled');
-        nextTab($active);
-    });
-    $(".prev-step").click(function (e) {
-        var $active = $('.wizard .nav-tabs li.active');
-        prevTab($active);
-    });
-
-    function nextTab(elem) {
-        $(elem).next().find('a[data-toggle="tab"]').click();
     }
-    function prevTab(elem) {
-        $(elem).prev().find('a[data-toggle="tab"]').click();
+});
+$(".next-step").click(function (e) {
+    let active = $('.wizard .nav-tabs li.active');
+    active.next().removeClass('disabled');
+    nextTab(active);
+});
+$(".prev-step").click(function (e) {
+    let active = $('.wizard .nav-tabs li.active');
+    prevTab(active);
+});
+
+function nextTab(elem) {
+    $(elem).next().find('a[data-toggle="tab"]').click();
+}
+function prevTab(elem) {
+    $(elem).prev().find('a[data-toggle="tab"]').click();
+}
+
+/**
+  * Permet de cliquer sur une ligne du tableau
+  * Redirige vers la Vue correspondante
+  */
+
+$('tbody').on("click", "tr", function () {
+    const npatient = $(this).attr('data-row-id');
+if (npatient === undefined)
+    return;
+    const pathArray = window.location.pathname.split("/");
+    let url = window.location.protocol + "//" + window.location.host;
+    for (let i = 0; i < pathArray.length - 1; i++) {
+        url += pathArray[i] + "/";
     }
-
-	/**
-	  * Permet de cliquer sur une ligne du tableau
-	  * Redirige vers la Vue correspondante
-	  */
-
-    $('tbody').on("click", "tr", function () {
-        var npatient = $(this).attr('data-row-id');
-	if (npatient === undefined)
-		return;
-        var pathArray = window.location.pathname.split("/");
-        var url = window.location.protocol + "//" + window.location.host;
-        for (var i = 0; i < pathArray.length - 1; i++) {
-            url += pathArray[i] + "/";
-        }
-        window.location.assign(url + "vue/" + npatient);
-    });
+    window.location.assign(url + "vue/" + npatient);
 });
